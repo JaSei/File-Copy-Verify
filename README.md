5582a60bc4a4455f404cec337a1375b51d6d5ee0
Jan Seidl <seidl@avast.com>
2016-09-26 12:28:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+File::Copy::Verify - It's new $module
+
+# SYNOPSIS
+
+    use File::Copy::Verify;
+
+# DESCRIPTION
+
+File::Copy::Verify is ...
+
+# LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan Seidl <seidl@avast.com>
---
+++ lib/File/Copy/Verify.pm
@@ -0,0 +1,39 @@
+package File::Copy::Verify;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+File::Copy::Verify - It's new $module
+
+=head1 SYNOPSIS
+
+    use File::Copy::Verify;
+
+=head1 DESCRIPTION
+
+File::Copy::Verify is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan Seidl.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Seidl E<lt>seidl@avast.comE<gt>
+
+=cut
+
